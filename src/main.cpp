7775cd54cf7229e4aee90d55adbc19f8d78f70da
Troy Gilman <troygilman@gmail.com>
2022-06-20 16:57:32 -0400

    abstracted VertexArray to class

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Renderer.hpp"
 #include "VertexBuffer.hpp"
 #include "IndexBuffer.hpp"
+#include "VertexArray.hpp"
 using namespace std;
 
 
@@ -119,18 +120,15 @@ int main() {
         2, 3, 0
     };
 
-    unsigned int vao;
-    GLCall(glGenVertexArrays(1, &vao));
-    GLCall(glBindVertexArray(vao))
-
+    VertexArray va;
     VertexBuffer vb(positions, 2 * 4 * sizeof(float));
-
-    GLCall(glEnableVertexAttribArray(0));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0));
+    VertexBufferLayout layout;
+    layout.Push<float>(2);
+    va.AddBuffer(vb, layout);
 
     IndexBuffer ib(indices, 6);
 
-    ShaderProgramSource source = ParseShader("../res/shaders/Basic.shader");
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
@@ -154,8 +152,9 @@ int main() {
         GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-        GLCall(glBindVertexArray(vao))
+        va.Bind();
         ib.Bind();
+
 
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
